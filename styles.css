2a0143c5c815841dcff4f46c924c4c65ab2562bd
ThehanSanvidu <thehansanvidu27@gmail.com>
2024-11-12 21:49:05 +0530

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,22 @@
+/* styles.css */
+body, html {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #282c34;
+}
+
+.game-container {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+canvas {
+  width: 100%;
+  height: 100%;
+}
